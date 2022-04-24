d911654825269747034025aa606e0b8ace16222f
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2022-04-24 23:45:29 +0530

    added init_setup.sh

---
+++ init_setup.sh
@@ -0,0 +1,15 @@
+echo [$(date)]: "START"
+echo [$(date)]: "creating environment"
+conda create --prefix ./env python=3.7 -y
+echo [$(date)]: "activate environment"
+source activate ./env
+echo [$(date)]: "install requirements"
+pip install -r requirements.txt
+echo "# ${PWD}" > README.md
+echo [$(date)]: "first commit"
+git add .
+git commit -m "first commit"
+echo [$(date)]: "END"
+
+# to remove everything -
+# rm -rf env/ .gitignore conda.yaml README.md .git/
