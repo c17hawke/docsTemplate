8a82ae838db9951d749d5dba0abd0b440d77b3ba
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2022-06-14 15:20:11 +0530

    mathjax added

--- init_setup.sh
+++ init_setup.sh
@@ -1,14 +1,11 @@
 echo [$(date)]: "START"
-echo [$(date)]: "creating environment"
-conda create --prefix ./env python=3.7 -y
+export _VERSION_=3.8
+echo [$(date)]: "creating environment with python ${_VERSION_}"
+conda create --prefix ./env python=${_VERSION_} -y
 echo [$(date)]: "activate environment"
 source activate ./env
 echo [$(date)]: "install requirements"
 pip install -r requirements.txt
-echo "# ${PWD}" > README.md
-echo [$(date)]: "first commit"
-git add .
-git commit -m "first commit"
 echo [$(date)]: "END"
 
 # to remove everything -
